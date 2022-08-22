dac1a347b1c666fe55384d808371645c77d262a8
ssavane26 <savaneprod@gmail.com>
2022-08-21 20:03:28 -0400

    Added Array Questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,66 @@
-// TODO: Include packages needed for this application
+const { default: inquirer } = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// TODO: Include packages needed for this application
+const inquirer = require ('inquirer')
+const fs = require ('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
+
+// Created an array of questions for user input, alongside a function to initialize app
+function init () {
+     inquirer.prompt([
+      {
+        type: 'input',
+        name: 'Name',
+        message: 'Enter Your GitHub username',
+      },
+      {
+        type: 'input',
+        name: 'Email',
+        message: 'Enter Email?',
+      },
+      {
+        type: 'input',
+        name: 'Name',
+        message: 'Enter Project Name',
+      },
+      {
+        type: 'input',
+        name: 'Description',
+        message: 'Enter Project Description',
+      },
+      {
+        type: 'input',
+        name: 'Username',
+        message: 'Enter Your GitHub Username!',
+      },
+      {
+        type: 'input',
+        name: 'Command',
+        message: 'Enter Desired Command To Install Dependencies!',
+      },
+      {
+        type: 'input',
+        name: 'Run',
+        message: 'Enter Command To Run Tests!',
+      },
+      {
+        type: 'input',
+        name: 'license',
+        message: 'Choose Your License!',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'none']
+      },
+      {
+        type: 'input',
+        name: 'Contribution',
+        message: 'How can others contribute to your project.',
+      },
+    ]);
+  }; 
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+
 
 // Function call to initialize app
 init();
--- README.md
+++ README.md
@@ -1,22 +1,5 @@
 # 09 Node.js Challenge: Professional README Generator
 
-## Your Task
-
-When creating an open source project on GitHub, it’s important to have a high-quality README for the app. This should include what the app is for, how to use the app, how to install it, how to report issues, and how to make contributions&mdash;this last part increases the likelihood that other developers will contribute to the success of the project. 
-
-You can quickly and easily create a README file by using a command-line application to generate one. This allows the project creator to devote more time to working on the project.
-
-Your task is to create a command-line application that dynamically generates a professional README.md file from a user's input using the [Inquirer package](https://www.npmjs.com/package/inquirer). Review the [Professional README Guide](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide) as a reminder of everything that a high-quality, professional README should contain. 
-
-The application will be invoked by using the following command:
-
-```bash
-node index.js
-```
-
-Because this application won’t be deployed, you’ll also need to provide a link to a walkthrough video that demonstrates its functionality. Revisit the Screencastify Tutorial in the prework as a refresher on how to record video from your computer. You’ll need to submit a link to the video _and_ add it to the README of your project.
-
-
 ## User Story
 
 ```md
