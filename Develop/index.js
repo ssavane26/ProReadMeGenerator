e663e38144933328745e3e86990c6cd864dedede
ssavane26 <savaneprod@gmail.com>
2022-08-23 19:55:11 -0400

    edited typos in js

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,3 @@
-const { default: inquirer } = require("inquirer");
 
 // TODO: Include packages needed for this application
 const inquirer = require ('inquirer')
@@ -11,69 +10,64 @@ function init () {
       {
         type: 'input',
         name: 'Name',
-        message: 'Enter Your GitHub username',
+        message: 'Enter Your GitHub username:',
       },
       {
         type: 'input',
         name: 'Email',
-        message: 'Enter Email?',
+        message: 'Enter Email?:',
       },
       {
         type: 'input',
-        name: 'Name',
-        message: 'Enter Project Name',
+        name: 'Title',
+        message: 'Enter Project Title:',
       },
       {
         type: 'input',
         name: 'Description',
-        message: 'Enter Project Description',
+        message: 'Enter Project Description (Please):',
       },
       {
         type: 'input',
-        name: 'Username',
-        message: 'Enter Your GitHub Username!',
+        name: 'Use',
+        message: 'Enter how this application should be used!:',
       },
       {
         type: 'input',
         name: 'Command',
-        message: 'Enter Desired Command To Install Dependencies!',
+        message: 'Enter Desired Command To Install Dependencies!:',
       },
       {
         type: 'input',
-        name: 'Run',
-        message: 'Enter Command To Run Tests!',
+        name: 'Test',
+        message: 'Enter Command To Run Tests!:',
       },
       {
-        type: 'input',
+        type: 'list',
         name: 'license',
-        message: 'Choose Your License!',
-        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'none']
+        message: 'Choose Your License!:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'Unlicensed']
       },
       {
         type: 'input',
         name: 'Contribution',
-        message: 'How can others contribute to your project.',
+        message: 'How can others contribute to your project?:',
       },
     ])
 
-// TODO: Create a function to write README file
-.then((data) => {
-  const fileName = generate(data)
-  writeToFile(fileName, data)
-  console.log('README Created!')
-  
- 
-
- 
-   
-    
-
+  .then((data) => {
+        
+    const fileName = generateMarkdown(data)
+    writeToFile(fileName, data)
+    console.log('README.md Created!')
    })
-}
- 
 
-// Function call to initialize app
-init();
+  }
+
+  // TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFileSync('README.md', fileName)
+  }
 
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -17,17 +17,69 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT"){
+    return `(https://opensource.org/licenses/MIT)`
+  }
+  if (license === "Apache 2.0"){
+    return `(https://opensource.org/licenses/Apache-2.0)`
+  }
+  if (license === "Mozilla Public License 2.0"){
+    return `(https://opensource.org/licenses/MPL-2.0)`
+  }
+  else if (license === "none"){
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license === "MIT") {
+    return `# License`
+  }
+  if(license === "Apache 2.0") {
+    return `# License`
+  }
+  if(license === "Mozilla Public License 2.0") {
+    return `# License`
+  }
+  else if (license ===  "Unlicensed"){
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return (
+     `# ${data.title}
+  ${renderLicenseBadge(data.license)}${renderLicenseLink(data.license)}
+  # Desctiption
+  ${data.Description}
+  # Table of Contents
+    * [Installation](#Installation)
+    * [Usage](#Usage)
+    * [License](#License)
+    * [Tests](#Tests)
+    * [Questions](#Questions)
+    * [Contributing](#Contributing)
+  # Installation
+  ${data.Installation}
+  # Usage
+  ${data.Usage}
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseBadge(data.license)}${renderLicenseLink(data.license)}
+  # Contributions
+  ${data.Contribution}
+  # Tests
+  ${data.Test}
+  # Questions
+  Reach out at:
+  [![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/${data.Github})
+  [![Email badge](https://img.shields.io/badge/Email-${data.Email}-red.svg)](mailto:${data.Email})
+  `);
+  
+  }
 
-`;
-}
 
 module.exports = generateMarkdown;
